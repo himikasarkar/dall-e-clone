eaa87dfa9e7af6a1eba5a8aa950b1ba9a7e35de4
Himika Sarkar <himikas.work@gmail.com>
2023-03-22 22:30:34 +0530

    updated format to webp

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -31,7 +31,7 @@ router.route('/').get(async(req,res) => {
 router.route('/').post(async(req,res) => {
     try{
         const {name, prompt, photo} = req.body;
-        const photoUrl = await cloudinary.uploader.upload(photo);
+        const photoUrl = await cloudinary.uploader.upload(photo, {format: 'webp'});
     
         const newPost = await Post.create({
             name,
